6473787cd1b9bcee32ab41404cb0ef85a697692a
Ideneal <ideneal.ztl@gmail.com>
2020-05-08 17:55:58 +0200

    Init Bundle

---
+++ src/IdenealIOBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ideneal\Bundle\IOBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class IdenealIOBundle extends Bundle 
+{
+}
